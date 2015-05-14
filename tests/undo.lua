e05b928359a2a114480f2967b8be1821cf3cd040
David Given <dg@cowlark.com>
2015-05-14 20:43:36 +0000

    Let's not save the undo stack on disk --- it makes the files very big and slow and is of questionable use.

--- tests/undo.lua
+++ tests/undo.lua
@@ -10,32 +10,32 @@ Cmd.SplitCurrentParagraph()
 AssertEquals(2, #Document)
 AssertTableEquals({"Hello,", "world!"}, Document[1])
 AssertTableEquals({"\016"}, Document[2])
-AssertEquals(2, #Document.undostack)
-AssertEquals(0, #Document.redostack)
+AssertEquals(2, #Document._undostack)
+AssertEquals(0, #Document._redostack)
 
 Cmd.Undo()
 
 AssertEquals(1, #Document)
 AssertTableEquals({"Hello,", "\016"}, Document[1])
-AssertEquals(1, #Document.undostack)
-AssertEquals(1, #Document.redostack)
+AssertEquals(1, #Document._undostack)
+AssertEquals(1, #Document._redostack)
 
 Cmd.Undo()
 
 AssertEquals(1, #Document)
 AssertTableEquals({""}, Document[1])
-AssertEquals(0, #Document.undostack)
-AssertEquals(2, #Document.redostack)
+AssertEquals(0, #Document._undostack)
+AssertEquals(2, #Document._redostack)
 
 Cmd.Redo()
 
 AssertEquals(1, #Document)
 AssertTableEquals({"Hello,", "\016"}, Document[1])
-AssertEquals(1, #Document.undostack)
-AssertEquals(1, #Document.redostack)
+AssertEquals(1, #Document._undostack)
+AssertEquals(1, #Document._redostack)
 
 Cmd.Checkpoint()
 
-AssertEquals(2, #Document.undostack)
-AssertEquals(0, #Document.redostack)
+AssertEquals(2, #Document._undostack)
+AssertEquals(0, #Document._redostack)
 
