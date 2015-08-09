afaea096dde06dbf7c0021289d6cda803ab9ecdf
David Given <dg@cowlark.com>
2015-08-09 22:17:27 +0200

    Fix very old and embarrassing typo.

--- src/lua/addons/docsetman.lua
+++ src/lua/addons/docsetman.lua
@@ -105,7 +105,7 @@ function Cmd.ManageDocumentsUI()
 		Form.Label {
 			x1 = 1, y1 = 1,
 			x2 = -1, y2 = 1,
-			value = "Select heading to jump to:"
+			value = "Select document:"
 		},
 		
 		Form.Label {
