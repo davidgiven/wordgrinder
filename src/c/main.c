6cc647c4d26770ea8938cfaa34948ab7c1742dc7
David Given <dg@cowlark.com>
2017-07-01 18:31:31 +0200

    Try several different locales, falling back to "", to attempt to work on systems which don't have C.UTF-8.

--- src/c/main.c
+++ src/c/main.c
@@ -8,9 +8,29 @@
 #include <locale.h>
 #include "globals.h"
 
+static void findlocale(void)
+{
+	static const char* locales[] =
+		{
+			"C.UTF-8",
+			"en_US.UTF-8",
+			"en_GB.UTF-8",
+			"",
+			NULL
+		};
+	const char** p = locales;
+
+	while (*p)
+	{
+		if (setlocale(LC_ALL, *p))
+			return;
+		p++;
+	}
+}
+
 int main(int argc, const char* argv[])
 {
-	setlocale(LC_ALL, "C.UTF-8");
+	findlocale();
 	script_init();
 	screen_init(argv);
 	word_init();
