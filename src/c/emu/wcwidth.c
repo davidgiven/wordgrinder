04aa34d62cb6157c23d7ce74f7ff8ea152c451c4
David Given <dg@cowlark.com>
2017-11-02 13:38:20 +0100

    Fix some license documentation issues. Move all the licenses into a licenses subdirectory for ease of organisation.

--- src/c/emu/wcwidth.c
+++ src/c/emu/wcwidth.c
@@ -1,6 +1,8 @@
-/* © 2008 David Given.
- * WordGrinder is licensed under the MIT open source license. See the COPYING
- * file in this distribution for the full text.
+/* © 2007 Markus Kuhn
+ * (modified by dg@cowlark.com)
+ * Permission to use, copy, modify, and distribute this software
+ * for any purpose and without fee is hereby granted. The author
+ * disclaims all warranties with regard to this software.
  */
 
 #include "globals.h"
