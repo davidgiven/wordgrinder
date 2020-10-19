b789c20a83465a2091cdd599a115ecc213898c26
David Given <dg@cowlark.com>
2020-10-19 22:18:50 +0200

    Install the MIME file; fix a desktop file linter error.

--- build.lua
+++ build.lua
@@ -601,6 +601,7 @@ if want_frontend("x11") or want_frontend("curses") then
     install_file("644", "bin/xwordgrinder.1", DESTDIR..MANDIR.."/man1/xwordgrinder.1")
     install_file("644", "README.wg", DESTDIR..DOCDIR.."/wordgrinder/README.wg")
     install_file("644", "extras/wordgrinder.desktop", DESTDIR..SHAREDIR.."/applications/wordgrinder.desktop")
+    install_file("644", "extras/wordgrinder.mime", DESTDIR..SHAREDIR.."/mime-info/wordgrinder.mime")
     install_file("644", "extras/icon.png", DESTDIR..SHAREDIR.."/pixmaps/wordgrinder.png")
 
     emit("build bin/wordgrinder.1: manpage wordgrinder.man")
