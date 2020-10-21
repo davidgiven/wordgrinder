5d58873ee43ae097e824413fe9ba07611a659676
David Given <dg@cowlark.com>
2020-10-21 18:46:17 +0200

    Show the current paragraph style on the status bar.

--- src/lua/addons/statusbar_position.lua
+++ src/lua/addons/statusbar_position.lua
@@ -12,7 +12,10 @@ do
 		terms[#terms+1] = 
 			{
 				priority=100,
-				value=string_format("P: %d/%d", Document.cp, #Document)
+				value=string_format("%s: %d/%d",
+					Document[Document.cp].style,
+					Document.cp,
+					#Document)
 			}
 	end
 	
