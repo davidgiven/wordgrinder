9e25779c75d21ae8cdce5fcc90704565c8f28fee
David Given <dg@cowlark.com>
2021-09-25 17:51:48 +0100

    Make endianness.h actually work on Windows.

--- src/c/arch/sdl/endianness.h
+++ src/c/arch/sdl/endianness.h
@@ -1,7 +1,7 @@
 #ifndef ENDIANNESS_H
 #define ENDIANNESS_H
 
-#if (defined(_WIN16) || defined(_WIN32) || defined(_WIN64)) && !defined(__WINDOWS__)
+#if defined(WIN32)
 	#define IS_LITTLE_ENDIAN
 #endif
 
