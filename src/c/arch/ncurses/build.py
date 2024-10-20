7e2ef0c711e38714612325e65c6e1ef567994110
David Given <dg@cowlark.com>
2024-10-20 22:53:41 +0200

    Simplify the build scripts and export more stuff.

--- src/c/arch/ncurses/build.py
+++ src/c/arch/ncurses/build.py
@@ -1,16 +1,18 @@
 from build.c import cxxlibrary
 from build.pkg import package
+from config import HAS_NCURSES
 
-package(name="libncursesw", package="ncursesw")
+if HAS_NCURSES:
+    package(name="libncursesw", package="ncursesw")
 
-cxxlibrary(
-    name="ncurses",
-    srcs=["./dpy.cc"],
-    deps=[
-        ".+libncursesw",
-        "src/c+globals",
-        "src/c/luau-em",
-        "third_party/libstb",
-        "third_party/luau",
-    ],
-)
+    cxxlibrary(
+        name="ncurses",
+        srcs=["./dpy.cc"],
+        deps=[
+            ".+libncursesw",
+            "src/c+globals",
+            "src/c/luau-em",
+            "third_party/libstb",
+            "third_party/luau",
+        ],
+    )
